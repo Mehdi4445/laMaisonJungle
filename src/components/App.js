8c818e6cc163ffd3e5b0c9531ec81cc3add4a807
Mehdi <mehdi.medimegh@isamm.u-manouba.tn>
2022-07-14 17:18:32 +0100

    first

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../App.css';
+import Banner from '../components/Banner';
+import Cart from '../components/Cart';
+
+function App() {
+  return <div> <Banner />
+          <Cart /></div>
+}
+
+
+export default App;
